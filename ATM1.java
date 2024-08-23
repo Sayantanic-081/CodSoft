e4dc4481d8f32b7fe4b1594e0d61b91df829c00d
Sayantanic-081 <sayantanic29@gmail.com>
2024-08-23 22:17:33 +0530

    Update ATM1.java

--- ATM1.java
+++ ATM1.java
@@ -14,7 +14,7 @@ class check_balance extends Action{
     void check(){
         Scanner sc=new Scanner(System.in);
         
-        System.out.println("Want to check balance? ");
+        System.out.println("Want to check balance?(y/n) ");
         char ch=sc.next().charAt(0);
         //System.out.println("Want to continue your transaction?");
         
@@ -22,9 +22,12 @@ class check_balance extends Action{
         System.out.println("Your current balance is: "+amt1);
         }
         
-        else{
+        else if(ch=='n'){
             System.out.println("Thank you");
             System.exit(0);
+        }
+        else{
+            System.out.println("Sorry!! Wrong Character");
         }
         
         
@@ -37,8 +40,10 @@ class check_balance extends Action{
 public class ATM1{
     
     public static void main(String args[]){
-        int a,amt,pin,or_amt=0;
+        int a,amt,pin,pin1,or_amt=0;
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a pin to access your account:");
+        pin1=sc.nextInt();
         check_balance ob= new check_balance();
           while(true){
         System.out.println("Enter\n1. Withdraw\n2. Deposit\n3. Check balance\n4. Exit");
@@ -53,14 +58,14 @@ public class ATM1{
                  continue;
              }
              if(amt>5000){
-                 System.out.println("Amount is too high to withdraw");
+                 System.out.println("Amount is too high to withdraw(it should be less than 5000Rs.)");
                  
              }
              else {
              System.out.println("Enter the pin: ");
              pin=sc.nextInt();
              
-             if(pin!=333){
+             if(pin!=pin1){
                  System.out.println("Wrong pin");
               
              }
@@ -75,12 +80,12 @@ public class ATM1{
                 System.out.println("Enter the amount");
              amt=sc.nextInt();
              if(amt>5000){
-                 System.out.println("Amount is too high to deposit");
+                 System.out.println("Amount is too high to deposit(it should be less than 5000Rs.)");
              }
              else{
              System.out.println("Enter the pin: ");
              pin=sc.nextInt();
-             if(pin!=333){
+             if(pin!=pin1){
                  System.out.println("Wrong pin");
               
              }
